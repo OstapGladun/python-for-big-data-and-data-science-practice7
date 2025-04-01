6617c9643ea96a155b13c90cafac71408e9b0346
Ostap Gladun <hladun.ostap@gmail.com>
2025-04-01 18:03:08 +0300

    Empty functions added

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+import pandas as pdg
+
+def read_from_console():
+    pass
+
+def read_from_file_builtin(file_path: str):
+    pass
+
+def read_from_file_pandas(file_path: str):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def write_to_console(text: str):
+    pass
+
+def write_to_file_builtin(file_path: str, text: str):
+    pass
